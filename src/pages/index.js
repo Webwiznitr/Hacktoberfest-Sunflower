07c6e5f0e991e4516dc36d7492ba72d6cfdf82f5
kumarshantanu01 <shantanuk280@gmail.com>
2021-10-16 16:27:16 +0530

    about me section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import AboutUs from "./aboutus"
 
 const IndexPage = () => (
   
     <>
       <Layout>
         <Seo title="Home" />
+        <AboutUs/>
       </Layout>
     </>
   
